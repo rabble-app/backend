7cf51a1b3095c2218edd9b699f9b68ff71ecaa4d
jiokeokwuosa <okwuosachijioke@gmail.com>
2023-04-17 05:50:55 +0100

    resolved lambda issue...

--- webpack/webpack.config.production.js
+++
@@ -1,6 +0,0 @@
-/* eslint-disable @typescript-eslint/no-var-requires *//* eslint-disable prettier/prettier */
-var base = require("./webpack.config.base");
-
-module.exports = base.merge({
-    // devtool: 'source-map',
-});
