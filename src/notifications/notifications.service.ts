b43822a7755c806fc3da08b60a278667710d9e60
jiokeokwuosa <softwareking@Chijiokes-MacBook-Air.local>
2023-09-12 12:21:38 +0100

    update

--- src/notifications/notifications.service.ts
+++ src/notifications/notifications.service.ts
@@ -8,8 +8,7 @@ import * as firebase from 'firebase-admin';
 firebase.initializeApp({
   credential: firebase.credential.cert({
     projectId: process.env.FIREBASE_PROJECT_ID,
-    // eslint-disable-next-line prettier/prettier
-    privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/gm, "\n"),
+    privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
     clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
   }),
 });
