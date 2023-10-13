3f1cb8ca856c829fbc8e87f7768badd2162c15d2
jiokeokwuosa <softwareking@Chijiokes-MacBook-Air.local>
2023-10-13 15:30:36 +0100

    update

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,18 +1,15 @@
-import { Module, forwardRef } from '@nestjs/common';
+import { Module } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
 import { PrismaService } from '../prisma.service';
 import { UsersControllerExtension } from './users.controller.extension';
 import { JwtModule } from '@nestjs/jwt';
-import { PaymentModule } from '../../src/payment/payment.module';
 
 @Module({
   imports: [
     JwtModule.register({
       secret: process.env.JWT_SECRET,
     }),
-    // PaymentModule
-    // forwardRef(() => PaymentModule),
   ],
   controllers: [UsersController, UsersControllerExtension],
   providers: [UsersService, PrismaService],
