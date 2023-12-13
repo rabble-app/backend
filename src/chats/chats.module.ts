af750c173d6f267dac63608ebbb43c4b3de42ae2
jiokeokwuosa <softwareking@Chijiokes-MacBook-Air.local>
2023-12-13 13:17:11 +0100

    notification for chat

--- src/chats/chats.module.ts
+++ src/chats/chats.module.ts
@@ -3,9 +3,13 @@ import { ChatsService } from './chats.service';
 import { ChatsController } from './chats.controller';
 import { PrismaService } from '../../src/prisma.service';
 import { JwtService } from '@nestjs/jwt';
+import { NotificationsModule } from 'src/notifications/notifications.module';
+import { TeamsModule } from 'src/teams/teams.module';
+import { UsersModule } from 'src/users/users.module';
 
 @Module({
   controllers: [ChatsController],
   providers: [ChatsService, PrismaService, JwtService],
+  imports: [NotificationsModule, TeamsModule, UsersModule],
 })
 export class ChatsModule {}
