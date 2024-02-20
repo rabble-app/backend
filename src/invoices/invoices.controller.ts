1a105489371c44e64c62cb27362395c4ebedb944
Chinedu Orie <nedsoftdeveloper@gmail.com>
2024-02-20 21:07:36 +0000

    chore: add documentation

--- src/invoices/invoices.controller.ts
+++ src/invoices/invoices.controller.ts
@@ -9,19 +9,54 @@ import {
 import type { Response } from 'express';
 import { InvoiceService } from './invoices.service';
 import { AuthGuard } from 'auth/auth.guard';
+import {
+  ApiBadRequestResponse,
+  ApiInternalServerErrorResponse,
+  ApiOkResponse,
+  ApiParam,
+  ApiTags,
+  ApiResponse,
+  ApiProduces,
+  ApiHeader,
+  ApiBearerAuth,
+} from '@nestjs/swagger';
 
+@ApiTags('invoices')
 @Controller('invoices')
+@ApiBearerAuth()
+@ApiProduces('application/pdf')
 export class InvoiceController {
   constructor(private readonly invoiceService: InvoiceService) {}
 
   @UseGuards(AuthGuard)
   @Get('/:orderId/:producerId')
+  @ApiParam({ name: 'orderId', type: String, required: true })
+  @ApiParam({ name: 'producerId', type: String, required: true })
+  @ApiOkResponse({ description: 'Invoice generated successfully' })
+  @ApiInternalServerErrorResponse({ description: 'Internal server error' })
+  @ApiBadRequestResponse({
+    description: 'Order ID and Producer ID are required',
+  })
+  @ApiResponse({
+    status: HttpStatus.OK,
+    description: 'PDF Document',
+    type: 'application/pdf',
+  })
+  @ApiHeader({
+    name: 'Authorization',
+    description: 'Bearer <access_token>',
+  })
   async getInvoices(
     @Param('orderId') orderId: string,
     @Param('producerId') producerId: string,
     @Res() res: Response,
   ) {
     try {
+      if (!orderId || !producerId) {
+        return res
+          .status(HttpStatus.BAD_REQUEST)
+          .json({ message: 'Order ID and Producer ID are required' });
+      }
       const pdfDoc = await this.invoiceService.generateInvoice(
         producerId,
         orderId,
