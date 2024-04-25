fa101d364c6f76f6a199371abfeb13c30003106e
Chinedu Orie <nedsoftdeveloper@gmail.com>
2024-04-25 20:55:33 +0100

    test: get partner inbound deliveries

--- test/stores/stores.e2e-spec.ts
+++ test/stores/stores.e2e-spec.ts
@@ -3,10 +3,10 @@ import { INestApplication, ValidationPipe } from '@nestjs/common';
 import request from 'supertest';
 import { AppModule } from '../../src/app.module';
 import { PrismaService } from '../../src/prisma.service';
-import { User } from '@prisma/client';
+import { Order, User } from '@prisma/client';
 import { faker } from '@faker-js/faker';
 import { AuthService } from '../../src/auth/auth.service';
-
+import { truncateDB } from '../truncate-db';
 describe('StoreController (e2e)', () => {
   let app: INestApplication;
   let prisma: PrismaService;
@@ -18,6 +18,7 @@ describe('StoreController (e2e)', () => {
   const testTime = 120000;
   let jwtToken: string;
   let storeId: string;
+  let order: Order;
 
   const store = {
     name: faker.internet.userName(),
@@ -54,7 +55,6 @@ describe('StoreController (e2e)', () => {
 
     await app.init();
     await app.listen(process.env.PORT);
-
     // create dummy user for test
     user = await prisma.user.create({
       data: {
@@ -62,17 +62,86 @@ describe('StoreController (e2e)', () => {
       },
     });
     userId = user.id;
+    const producer = await prisma.producer.create({
+      data: {
+        userId,
+        businessName: faker.internet.userName(),
+      },
+    });
+
+    // create  team for test
+    const team = await prisma.buyingTeam.create({
+      data: {
+        name: faker.internet.userName(),
+        postalCode: '234-54',
+        hostId: user.id,
+        frequency: 604800,
+        description: 'Dummy description',
+        isPublic: true,
+        nextDeliveryDate: new Date(),
+        producerId: producer.id,
+      },
+    });
+
+    const categoryOption = await prisma.producerCategoryOption.create({
+      data: {
+        name: faker.lorem.word({ length: 10 }),
+      },
+    });
+    await prisma.producerCategory.create({
+      data: {
+        producerId: producer.id,
+        producerCategoryOptionId: categoryOption.id,
+      },
+    });
+
+    // await prisma.shipping.create({
+    //   data: {
+    //     userId: user.id,
+    //     buildingNo: '123',
+    //     address: 'dummy address',
+    //     city: 'dummy city',
+    //   },
+    // });
+
+    // create  order for test
+    order = await prisma.order.create({
+      data: {
+        teamId: team.id,
+        minimumTreshold: 50,
+        deliveryDate: new Date(),
+      },
+    });
+    const productCategory = await prisma.productCategory.create({
+      data: {
+        name: faker.lorem.word({ length: 15 }),
+      },
+    });
+    const product = await prisma.product.create({
+      data: {
+        producerId: producer.id,
+        name: 'dummy product',
+        price: 100,
+        categoryId: productCategory.id,
+      },
+    });
+
+    await prisma.basket.create({
+      data: {
+        orderId: order.id,
+        price: 100,
+        quantity: 2,
+        userId,
+        productId: product.id,
+      },
+    });
 
     // create dummy token
     jwtToken = authService.generateToken({ userId });
   }, testTime);
 
   afterAll(async () => {
-    await prisma.user.delete({
-      where: {
-        id: userId,
-      },
-    });
+    await truncateDB(prisma);
     await app.close();
   });
 
@@ -167,6 +236,58 @@ describe('StoreController (e2e)', () => {
         expect(response.body).toHaveProperty('data');
         expect(response.body.error).toBeUndefined();
         expect(typeof response.body.data).toBe('object');
+      },
+      testTime,
+    );
+    it(
+      '/store/(Get) should get store inbound deliveries for today successfully',
+      async () => {
+        const response = await request(app.getHttpServer())
+          .get(`/store/${storeId}/deliveries?period=today`)
+          .set('Authorization', `Bearer ${jwtToken}`)
+          .send({ city: 'London' })
+          .expect(200);
+        expect(response.body).toHaveProperty('data');
+        expect(response.body.error).toBeUndefined();
+        expect(response.body.data).toHaveLength(1);
+      },
+      testTime,
+    );
+    it(
+      '/store/(Get) should get past store inbound deliveries successfully',
+      async () => {
+        const pastDate = new Date();
+        pastDate.setDate(pastDate.getDate() - 1);
+        await prisma.order.update({
+          where: { id: order.id },
+          data: { deliveryDate: pastDate },
+        });
+        const response = await request(app.getHttpServer())
+          .get(`/store/${storeId}/deliveries?period=past`)
+          .set('Authorization', `Bearer ${jwtToken}`)
+          .expect(200);
+        expect(response.body).toHaveProperty('data');
+        expect(response.body.error).toBeUndefined();
+        expect(response.body.data).toHaveLength(1);
+      },
+      testTime,
+    );
+    it(
+      '/store/(Get) should get upcoming store inbound deliveries successfully',
+      async () => {
+        const futureDate = new Date();
+        futureDate.setDate(futureDate.getDate() + 1);
+        await prisma.order.update({
+          where: { id: order.id },
+          data: { deliveryDate: futureDate },
+        });
+        const response = await request(app.getHttpServer())
+          .get(`/store/${storeId}/deliveries?period=upcoming&search=dummy`)
+          .set('Authorization', `Bearer ${jwtToken}`)
+          .expect(200);
+        expect(response.body).toHaveProperty('data');
+        expect(response.body.error).toBeUndefined();
+        expect(response.body.data).toHaveLength(1);
       },
       testTime,
     );
