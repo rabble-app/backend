adccec919acc58d3e89c31531448270895068f40
jiokeokwuosa <softwareking@Chijiokes-MacBook-Air.local>
2024-05-28 14:23:34 +0100

    feat: user onboarding tracker

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -80,7 +80,11 @@ export class UsersService {
       });
     }
 
-    if (result.role == 'PARTNER' && result.onboardingStage == 1) {
+    if (
+      result.role == 'PARTNER' &&
+      result.firstName != null &&
+      !data.onboardingStage
+    ) {
       await this.prisma.user.update({
         where: { id: result.id },
         data: { onboardingStage: 2 },
