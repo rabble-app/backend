6f4104acfe3fcdba9a280e991a0426dd93150d45
jiokeokwuosa <softwareking@Chijiokes-MacBook-Air.local>
2024-07-11 17:34:07 +0100

    update

--- src/uploads/uploads.service.ts
+++ src/uploads/uploads.service.ts
@@ -14,7 +14,7 @@ export class UploadsService {
       accessKeyId: this.parameters.RABBLE_AWS_BUCKET_ACCESS_KEY_ID,
       secretAccessKey: this.parameters.RABBLE_AWS_BUCKET_SECRET_ACCESS_KEY,
     },
-    region: this.parameters.RABBLE_AWS_BUCKET_REGION,
+    region: 'us-east-2',
   });
 
   async uploadFile(
