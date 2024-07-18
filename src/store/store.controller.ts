124f295755d36f8ea788192d2a0030e5a865206a
jiokeokwuosa <softwareking@Chijiokes-MacBook-Air.local>
2024-07-18 14:31:08 +0100

    update

--- src/store/store.controller.ts
+++ src/store/store.controller.ts
@@ -404,10 +404,10 @@ export class StoreController {
     @Param('storeId') storeId: string,
     @Res({ passthrough: true }) res: Response,
     @Request() req,
-    @Query('offset') offset?: number,
-    @Query('limit') limit?: number,
-    @Query('period') period?: 'today' | 'upcoming' | 'completed',
     @Query('search') search?: string,
+    @Query('limit') limit?: number,
+    @Query('offset') offset?: number,
+    @Query('period') period?: 'today' | 'upcoming' | 'completed',
   ): Promise<IAPIResponse> {
     const { store, skip, take } =
       await this.storeService.storeDeliveryAndCollectionValidation(
