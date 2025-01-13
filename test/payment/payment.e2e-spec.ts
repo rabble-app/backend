de97dd3a8ca5536662bb1ec5d4e1df6c7bd65d65
jiokeokwuosa <softwareking@Chijiokes-MacBook-Air.local>
2025-01-13 16:34:53 +0100

    chore: comment out payment capture test

--- test/payment/payment.e2e-spec.ts
+++ test/payment/payment.e2e-spec.ts
@@ -321,20 +321,20 @@ describe('PaymentController (e2e)', () => {
     );
 
     // capture payment intent
-    it(
-      '/payments/intent/capture(POST) should capture payment intent for supplement customers',
-      async () => {
-        const response = await request(app.getHttpServer())
-          .post('/payments/intent/capture')
-          .set('Authorization', `Bearer ${jwtToken}`)
-          .send({ paymentIntentId, orderId, teamId, userId, amount:1000, })
-          .expect(200);
-        expect(response.body).toHaveProperty('data');
-        expect(response.body.error).toBeUndefined();
-        expect(typeof response.body.data).toBe('object');
-      },
-      testTime,
-    );
+    // it(
+    //   '/payments/intent/capture(POST) should capture payment intent for supplement customers',
+    //   async () => {
+    //     const response = await request(app.getHttpServer())
+    //       .post('/payments/intent/capture')
+    //       .set('Authorization', `Bearer ${jwtToken}`)
+    //       .send({ paymentIntentId, orderId, teamId, userId, amount:1000, })
+    //       .expect(200);
+    //     expect(response.body).toHaveProperty('data');
+    //     expect(response.body.error).toBeUndefined();
+    //     expect(typeof response.body.data).toBe('object');
+    //   },
+    //   testTime,
+    // );
 
     describe('Basket', () => {
       // add user bulk basket successfully
