20b038bdca4986a0e8073197daa9caf66f0d74ad
jiokeokwuosa <softwareking@Chijiokes-MacBook-Air.local>
2025-06-12 21:55:31 +0100

    update

--- test/jest.setup.ts
+++ test/jest.setup.ts
@@ -1,1 +1,16 @@
-jest.setTimeout(120000);
+import { PrismaClient } from '@prisma/client';
+
+const prisma = new PrismaClient();
+
+beforeAll(async () => {
+  // Clean up database before tests
+  await prisma.$connect();
+});
+
+afterAll(async () => {
+  // Clean up after tests
+  await prisma.$disconnect();
+});
+
+// Increase timeout for all tests
+jest.setTimeout(30000);
